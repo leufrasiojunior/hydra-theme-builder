ee7d9037d21d91a1a08f0c0a7d4e2e7776592352
Leonald <Leonald.junior@infracommerce.com.br>
2025-06-04 22:38:04 -0300

    edit url to show featured games

--- src/pages/api/hydra/featured.ts
+++ src/pages/api/hydra/featured.ts
@@ -1,10 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   try {
-    const response = await fetch('https://hydra-api-us/games/featured');
+    const response = await fetch(
+      'https://hydra-api-us-east-1.losbroxas.org/games/featured'
+    );
     if (!response.ok) {
-      return res.status(response.status).json({ error: 'Failed to fetch Hydra data' });
+      return res
+        .status(response.status)
+        .json({ error: 'Failed to fetch Hydra data' });
     }
     const data = await response.json();
     res.status(200).json(data);
